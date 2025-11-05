0d676026ed50f6e9fa3c170b6fd21be6f594ff35
David Nadlinger <code@klickverbot.at>
2025-11-05 18:45:03 +0000

    complex_colorize: Update for colorio API change

--- src/complex_colorize.jl
+++ src/complex_colorize.jl
@@ -9,6 +9,17 @@ function make_cam16ucs()
     pyimport("colorio.cs").CAM16UCS(0.69, 20, 64 / π / 5)
 end
 
+function make_srgb1()
+    pyimport("colorio.cs").SRGB1()
+end
+
+function colorize_cam16(complex_values; max_magnitude=nothing)
+    if max_magnitude === nothing
+        max_magnitude = maximum(abs.(complex_values))
+    end
+    colorize_cam16(abs.(complex_values) / max_magnitude, angle.(complex_values) / 2π)
+end
+
 function colorize_cam16(ampls, phases_turns)
     # Size of circle in CAM16UCS space. Constrained by size of SRGB gamut.
     r0 = 21.652
@@ -18,31 +29,43 @@ function colorize_cam16(ampls, phases_turns)
     # L = 0.5 phase circle.
     for idx in eachindex(IndexCartesian(), ampls)
         camdata[1, idx] = 100 * (2 - ampls[idx]) / 2
-        phase = 2π * phases_turns[idx] + 0.8π
+        phase = 2π * phases_turns[idx] - 0.75π
         camdata[2, idx] = r0 * ampls[idx] * cos(phase)
         camdata[3, idx] = r0 * ampls[idx] * sin(phase)
     end
     cam = make_cam16ucs()
-    clamp.(cam.to_rgb1(camdata), 0, 1)
+    srgb = make_srgb1()
+    clamp.(srgb.from_xyz100(cam.to_xyz100(camdata), mode="clip"), 0, 1)
 end
 
-function draw_cam16_ampl_bar(centre_x, centre_y, width, height, min, max, ax)
+function draw_cam16_ampl_bar(centre_x, centre_y, width, height, min, max, ax; direction=:vertical, fontsize=6)
     mc = pyimport("matplotlib.collections")
     mp = pyimport("matplotlib.patches")
 
     patches = []
     steps = 256
-    degree_each = 360 / steps
-    ys = centre_y .+ range(-height / 2, height / 2, length=steps + 1)[1:end - 1]
-    for y in ys
-        push!(patches, mp.Rectangle((centre_x - width / 2, y), width, height / steps, linewidth=0, edgecolor="none"))
+    if direction == :vertical
+        ys = centre_y .+ range(-height / 2, height / 2, length=steps + 1)[1:end - 1]
+        for y in ys
+            push!(patches, mp.Rectangle((centre_x - width / 2, y), width, height / steps,
+                linewidth=0, edgecolor="none"))
+        end
+    elseif direction == :horizontal
+        xs = centre_x .+ range(-width / 2, width / 2, length=steps + 1)[1:end - 1]
+        for x in xs
+            push!(patches, mp.Rectangle((x, centre_y - height / 2), width / steps, height,
+                linewidth=0, edgecolor="none"))
+        end
+    else
+        throw(ArgumentError("Invalid direction: '$(direction)'"))
     end
     
-    p = mc.PatchCollection(patches)
+    p = mc.PatchCollection(patches, linewidth=0, antialiased=false)
     camdata = zeros((3, steps))
     camdata[1, :] = range(100, 50, length=steps)
     cam = make_cam16ucs()
-    colors = clamp.(cam.to_rgb1(camdata), 0, 1)
+    srgb = make_srgb1()
+    colors = srgb.from_xyz100(cam.to_xyz100(camdata), mode="clip")
     p.set_facecolor(colors')
     ax.add_collection(p)
     
@@ -50,13 +73,28 @@ function draw_cam16_ampl_bar(centre_x, centre_y, width, height, min, max, ax)
         facecolor="none", edgecolor="k", linewidth=0.4)
     ax.add_patch(rect)
     
-    ax.text(centre_x, centre_y - height / 2 - 0.5, "$(min)", size=6, color=colors[:, end],
-        horizontalalignment="center", verticalalignment="top")
-    ax.text(centre_x, centre_y + height / 2, "$(max)", size=6, color=colors[:, end],
-        horizontalalignment="center", verticalalignment="bottom")
+    if direction == :vertical
+        ax.annotate("$(min)", (centre_x, centre_y - height / 2),
+            (0, -1), textcoords="offset points",
+            size=fontsize, color=colors[:, end],
+            horizontalalignment="center", verticalalignment="top")
+        ax.annotate("$(max)",  (centre_x, centre_y + height / 2),
+            (0, 0), textcoords="offset points",
+            size=fontsize, color=colors[:, end],
+            horizontalalignment="center", verticalalignment="bottom")
+    else
+        ax.annotate("$(min)", (centre_x - width / 2, centre_y),
+            (-2, 0), textcoords="offset points",
+            size=fontsize, color=colors[:, end],
+            horizontalalignment="right", verticalalignment="center")
+        ax.annotate("$(max)", (centre_x + width / 2, centre_y),
+            (3, 0), textcoords="offset points",
+            size=fontsize, color=colors[:, end],
+            horizontalalignment="left", verticalalignment="center")
+    end
 end
 
-function draw_cam16_phase_ring(x, y, radius, ax)
+function draw_cam16_phase_ring(x, y, radius, ax; fontsize=6)
     mc = pyimport("matplotlib.collections")
     mp = pyimport("matplotlib.patches")
 
@@ -67,17 +105,19 @@ function draw_cam16_phase_ring(x, y, radius, ax)
         push!(patches, mp.Wedge((x, y), radius,
             (i - 1) * degree_each, i * degree_each, width=radius / 2))
     end
-    p = mc.PatchCollection(patches)
+    p = mc.PatchCollection(patches, linewidth=0, antialiased=false)
     colors = colorize_cam16(ones(steps), collect(range(0, 1, length=steps + 1))[1:end - 1])
     p.set_facecolor(colors')
     ax.add_collection(p)
 
     padded = 1.1 * radius
     cam = make_cam16ucs()
-    color = cam.to_rgb1([cam.from_rgb1(colors[:, 1])[1], 0, 0])
-    y_fudge = -0.4
-    ax.text(x + padded, y + y_fudge, L"$+$", ha="left", va="center", size=6, color=color)
-    ax.text(x - padded, y + y_fudge, L"$-$", ha="right", va="center", size=6, color=color)
-    ax.text(x, y + padded + y_fudge, L"$\mathrm{i}$", ha="center", va="bottom", size=6, color=color)
-    ax.text(x, y - padded + y_fudge, L"$-\mathrm{i}$", ha="center", va="top", size=6, color=color)
+    srgb = make_srgb1()
+    one_cam = cam.from_xyz100(srgb.to_xyz100(colors[:, 1]))
+    color = srgb.from_xyz100(cam.to_xyz100([one_cam[1], 0, 0]))
+    ax.annotate(L"$+$", (x + padded, y), ha="left", va="center", size=fontsize, color=color)
+    ax.annotate(L"$-$", (x - padded, y), ha="right", va="center", size=fontsize, color=color)
+    # Corrections for Minion Pro via LaTeX; should probably find a better way.
+    ax.annotate(L"$\mathrm{i}$", (x, y + padded), (0, -fontsize / 8), textcoords="offset points", ha="center", va="bottom", size=fontsize, color=color)
+    ax.annotate(L"$-\mathrm{i}$", (x, y - padded), (-fontsize / 4, 0), textcoords="offset points", ha="center", va="top", size=fontsize, color=color)
 end
